a5e6293cb8ef8adea62d7d77151cd36c144eac56
Aaron Marks <nymacro@gmail.com>
2015-06-24 20:37:26 +1000

    Use Test.Hspec.Wai to test server

---
+++ server/Kmip/Server.hs
@@ -0,0 +1,147 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Kmip.Server where
+
+import Web.Scotty
+import Network.Wai
+
+import Data.Monoid (mconcat)
+import Data.Text.Lazy (pack)
+
+import Data.ByteString.Lazy (fromStrict, toStrict)
+import Data.ByteString.Base64
+
+import qualified Ttlv.Validator.Structures as S
+import Ttlv.Validator.Message
+import Ttlv.Parser
+
+import Ttlv.Tag as T
+import Ttlv.Data
+
+-- import Control.Lens
+
+import Text.Blaze (ToMarkup(..))
+import Text.Blaze.Html.Renderer.Text (renderHtml)
+import qualified Text.Blaze.Html5 as H
+import Text.Blaze.Html5 ((!))
+import Text.Blaze.Html5.Attributes
+
+import Stitch
+import Stitch.Combinators
+import qualified Data.Text as T
+
+mainTitle = "KMIP Tool"
+
+homePage = H.html $ do
+  H.head $ H.title mainTitle
+  H.body "Hello World!"
+
+stylesheet = renderCSS $ do
+  "body" ? do
+    "background-color" .= "#fffff0"
+    "color" .= "#303030"
+  ".tag" ? do
+    "border" .= "1px solid #303030"
+    "background-color" .= "#cccccc"
+    "margin" .= "8 auto"
+    "padding" .= "4px"
+  ".tagName" ? do
+    "display" .= "inline"
+    "font-size" .= "16"
+    "font-weight" .= "bold"
+    "padding" .= "4px"
+  ".data" ? do
+    "border" .= "1px dotted #606060"
+    "background-color" .= "#dddddd"
+    "margin" .= "8 auto"
+    "padding" .= "4px"
+  ".type" ? do
+    "display" .= "inline"
+    "font-style" .= "italic"
+    "border" .= "1px solid #a0a0a0"
+    "background-color" .= "#f0f0f0"
+    "margin-right" .= "16px"
+    "padding" .= "4px"
+  ".good" ? do
+    "display" .= "inline"
+    "background-color" .= "#60ff60"
+    "color" .= "#103010"
+    "border" .= "2px solid #308830"
+    "padding" .= "4px"
+    "font-size" .= "24"
+  ".bad" ? do
+    "display" .= "inline"
+    "background-color" .= "#ff6060"
+    "color" .= "#301010"
+    "border" .= "2px solid #883030"
+    "padding" .= "4px"
+    "font-size" .= "24"
+
+defaultThings :: H.Html -> H.Html
+defaultThings body_ = do
+  H.head $ do
+    H.title mainTitle
+    H.style ! type_ "text/css" $ H.text stylesheet
+  H.body body_
+    
+renderTtlv :: Ttlv -> H.Html
+renderTtlv t = H.html $ do
+  H.div ! class_ "tag" $ do
+    H.div ! class_ "tagName" $ H.string $ show (getTtlvTag t)
+    H.div ! class_ "data" $
+      case getTtlvData t of
+       (TtlvStructure  t) -> typeString "Structure"   >> mapM_ (renderTtlv) t
+       (TtlvInt        t) -> typeString "Int"         >> (H.string $ show t)
+       (TtlvLongInt    t) -> typeString "Long Int"    >> (H.string $ show t)
+       (TtlvBigInt     t) -> typeString "Big Int"     >> (H.string $ show t)
+       (TtlvEnum       t) -> typeString "Enum"        >> (H.string $ show t)
+       (TtlvBool       t) -> typeString "Bool"        >> (H.string $ show t)
+       (TtlvString     t) -> typeString "String"      >> (H.string $ show t)
+       (TtlvByteString t) -> typeString "Byte String" >> (H.string $ show t)
+       (TtlvDateTime   t) -> typeString "Date Time"   >> (H.string $ show t)
+       (TtlvInterval   t) -> typeString "Interval"    >> (H.string $ show t)
+  where typeString s = H.div ! class_ "type" $ H.string s
+
+server :: ScottyM ()
+server = do
+  get "/" $ do
+    html $ renderHtml $ homePage
+
+  get "/hello" $ do
+    html $ "Hello world"
+
+  post "/display" $ do
+    ttlv <- body
+    case decode $ toStrict ttlv of
+     Right x -> html . renderHtml $ do
+       H.h1 "KMIP Display"
+       let ttlv'       = decodeTtlv $ fromStrict x
+           validHtml   = H.div ! class_ "good" $ H.string "Validates"
+           invalidHtml = H.div ! class_ "bad"  $ H.string "Failed Validation"
+       either (\_ -> invalidHtml) (\_ -> validHtml) $ S.runTtlvParser (requestMessage S.<|> responseMessage) ttlv'
+       defaultThings $ renderTtlv ttlv'
+     Left _  -> html "bad input"
+
+  -- validate request or response
+  post "/validate" $ do
+    ttlv <- body
+    case decode $ toStrict ttlv of
+     Right x -> case S.runTtlvParser (requestMessage S.<|> responseMessage) (decodeTtlv $ fromStrict x) of
+                 Right _ -> html "ok"
+                 Left  e -> html $ pack $ mconcat (map (++ "\n") e)
+     Left _ -> html "bad input"
+
+  post "/validate/request" $ do
+    ttlv <- body
+    case decode $ toStrict ttlv of
+     Right x -> case S.runTtlvParser requestMessage (decodeTtlv $ fromStrict x) of
+                 Right _ -> html "ok"
+                 Left  e -> html $ pack $ mconcat (map (++ "\n") e)
+     Left _ -> html "bad input"
+
+  post "/validate/response" $ do
+    ttlv <- body
+    case decode $ toStrict ttlv of
+     Right x -> case S.runTtlvParser responseMessage (decodeTtlv $ fromStrict x) of
+                 Right _ -> html "ok"
+                 Left  e -> html $ pack $ mconcat (map (++ "\n") e)
+     Left _ -> html "bad input"
--- tests/Kmip/ServerSpec.hs
+++ tests/Kmip/ServerSpec.hs
@@ -2,6 +2,7 @@
 module Kmip.ServerSpec where
 
 import Test.Hspec
+import Test.Hspec.Wai
 
 import Network.URI
 import Network.TCP as TCP
@@ -16,26 +17,19 @@ import Data.Maybe (fromJust)
 
 import Kmip10Data
 
--- Network.HTTP doesn't like "localhost"
-validate ttlv = simpleHTTP $ req ttlv
-  where req t = Request { rqURI = fromJust $ parseURI "http://127.0.0.1:3000/validate"
-                        , rqMethod = POST
-                        , rqHeaders = [ mkHeader HdrContentType "application/x-www-form-urlencoded"
-                                      , mkHeader HdrContentLength (show $ length t)
-                                      ]
-                        , rqBody = t }
+import qualified Web.Scotty as S
+import qualified Kmip.Server
+
+import Debug.Trace (trace)
 
 spec :: Spec
-spec = do
+spec = with (S.scottyApp Kmip.Server.server) $ do
   describe "Server" $ do
     it "should respond to hello" $ do
-      rsp <- simpleHTTP (getRequest "http://127.0.0.1:3000/hello")
-      body <- fmap (take 100) (getResponseBody rsp)
-      body `shouldBe` "Hello world"
+      get "/hello" `shouldRespondWith` "Hello world" { matchStatus = 200 }
 
     -- run through all data
     describe "Validate" $ do
-      mapM_ (\x -> it ("should validate " ++ fst x) $ do
-                rsp <- validate $ unpack $ encode $ toStrict $ snd x
-                body <- fmap (take 100) (getResponseBody rsp)
-                body `shouldBe` "ok") kmip_1_0__all
+      mapM_ (\x -> it ("should validate " ++ fst x) $
+                   post "/validate" (fromStrict . encode . toStrict $ snd x) `shouldRespondWith` "ok" { matchStatus = 200 })
+        kmip_1_0__all
